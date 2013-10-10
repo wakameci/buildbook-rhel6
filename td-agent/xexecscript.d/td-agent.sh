a27099685cc3f0fdffe87eafd5e9629ce17cc9d4
Masahito Yoshida <masahito@axsh.net>
2013-10-11 01:05:37 +0900

    add td-agent.

---
+++ td-agent/xexecscript.d/td-agent.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+# Installing Fluentd Using rpm Package
+# - http://docs.fluentd.org/articles/install-by-rpm
+#
+set -e
+
+cat <<'EOS' > $1/etc/yum.repos.d/td.repo
+[treasuredata]
+name=TreasureData
+baseurl=http://packages.treasure-data.com/redhat/$basearch
+gpgcheck=0
+EOS
+
+# make sure to define td-agent version
+case "${td_agent_version}" in
+""|latest)
+   td_agent_version=
+   ;;
+*)
+   td_agent_version="-${td_agent_version}"
+   ;;
+esac
+
+chroot $1 $SHELL -ex <<EOS
+  yum repolist
+  yum install -y td-agent${td_agent_version}
+EOS
