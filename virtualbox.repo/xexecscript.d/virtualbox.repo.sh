25720a636ae46a9aee5218cb1f67ee748df3ac9e
Masahito Yoshida <masahito@axsh.net>
2013-11-03 23:11:37 +0900

    replace vmbuilder internal function with yum command.

---
+++ virtualbox.repo/xexecscript.d/virtualbox.repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+
+declare chroot_dir=$1
+
+chroot $1 $SHELL -ex <<'EOS'
+  if ! [[ -f /etc/yum.repos.d/virtualbox.repo ]]; then
+    curl -fsSkL http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -o /etc/yum.repos.d/virtualbox.repo
+  fi
+EOS
