9173f328524f8718e8cc170a1d6f0cd97b3d1aac
Masahito Yoshida <masahito@axsh.net>
2013-11-03 18:19:00 +0900

    disable "chkconfig on".

--- ntp/xexecscript.d/ntp.sh
+++ ntp/xexecscript.d/ntp.sh
@@ -9,9 +9,7 @@ declare chroot_dir=$1
 
 chroot $1 $SHELL -ex <<'EOS'
   yum install --disablerepo=updates -y ntp ntpdate
-EOS
 
-chroot $1 $SHELL -ex <<EOS
-  chkconfig ntpd    on
-  chkconfig ntpdate on
+ #chkconfig ntpd    on
+ #chkconfig ntpdate on
 EOS
