1b5aa421eb4b149360fdd5f3413fa287424b4960
Masahito Yoshida <masahito@axsh.net>
2013-11-03 15:41:14 +0900

    update zfs-release- to 1-3.

--- zfs-on-linux/xexecscript.d/zfs-on-linux.sh
+++ zfs-on-linux/xexecscript.d/zfs-on-linux.sh
@@ -3,13 +3,16 @@
 # requires:
 #  bash
 #
+# RHEL / CentOS / Scientific Linux Packages
+# - http://zfsonlinux.org/epel.html
+#
 set -e
 
 declare chroot_dir=$1
 
 chroot $1 $SHELL -ex <<'EOS'
   yum install --disablerepo=updates -y kernel-devel
-  yum localinstall -y --nogpgcheck http://archive.zfsonlinux.org/epel/zfs-release-1-2.el6.noarch.rpm
+  yum localinstall -y --nogpgcheck http://archive.zfsonlinux.org/epel/zfs-release-1-3.el6.noarch.rpm
   yum install --disablerepo=updates -y zfs
 
   chkconfig --list zfs
