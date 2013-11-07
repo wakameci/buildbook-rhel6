5d26b922c67b93c2651c7e0dd122555a8a3c052f
Masahito Yoshida <masahito@axsh.net>
2013-11-08 02:08:57 +0900

    add nginx.

---
+++ nginx/xexecscript.d/nginx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+
+declare chroot_dir=$1
+
+chroot $1 $SHELL -ex <<'EOS'
+  if [[ -f /etc/centos-release ]]; then
+    distro_name=centos
+  else
+    distro_name=rhel
+  fi
+
+  rpm -Uvh http://nginx.org/packages/${distro_name}/6/noarch/RPMS/nginx-release-${distro_name}-6-0.el6.ngx.noarch.rpm
+  yum install --disablerepo=updates -y install nginx
+EOS
