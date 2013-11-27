0cd4f2b526870bd1a749944a0e50e32cf6089ebb
Masahito Yoshida <masahito@axsh.net>
2013-11-28 00:29:50 +0900

    add sensu.

--- epel-release/xexecscript.d/epel-release.sh
+++ epel-release/xexecscript.d/epel-release.sh
@@ -8,5 +8,5 @@ set -e
 declare chroot_dir=$1
 
 chroot $1 $SHELL -ex <<'EOS'
-  yum install --disablerepo=updates -y http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm
+  rpm -qi epel-release-6-8 >/dev/null || { yum install --disablerepo=updates -y http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm; }
 EOS
