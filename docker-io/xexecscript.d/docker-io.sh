279086ef920408620cb797dd507054e20f86504f
Masahito Yoshida <masahito@axsh.net>
2013-11-28 11:36:46 +0900

    use if-statement.

--- docker-io/xexecscript.d/docker-io.sh
+++ docker-io/xexecscript.d/docker-io.sh
@@ -17,11 +17,11 @@ chroot $1 $SHELL -ex <<'EOS'
   bootdir_path=
   root_dev=$(awk '$2 == "/boot" {print $1}' /etc/fstab)
 
-  [[ -n "${root_dev}" ]] || {
+  if [[ -z "${root_dev}" ]]; then
     # has no /boot partition case
     root_dev=$(awk '$2 == "/" {print $1}' /etc/fstab)
     bootdir_path=/boot
-  }
+  fi
 
   grub_title="kernel-ml-aufs (${version})"
   cat <<-_EOS_ >> /boot/grub/grub.conf
@@ -35,9 +35,9 @@ chroot $1 $SHELL -ex <<'EOS'
   # *** "grep" should be used at after 'cat -n'. because ${grub_title} includes regex meta characters. ex. '(' and ')'. ***
   menu_order=$(egrep ^title /boot/grub/grub.conf | cat -n | grep "${grub_title}" | tail | awk '{print $1}')
   menu_offset=0
-  [[ -z "${menu_order}" ]] || {
+  if [[ -n "${menu_order}" ]]; then
     menu_offset=$((${menu_order} - 1))
-  }
+  fi
   sed -i "s,^default=.*,default=${menu_offset}," /boot/grub/grub.conf
   cat /boot/grub/grub.conf
 EOS
