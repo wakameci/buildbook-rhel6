51907a4e14cecb583d55efe10def995082fa3e14
Masahito Yoshida <masahito@axsh.net>
2014-07-01 20:00:02 +0900

    vagrant 1.4.1 -> 1.6.3.

--- vagrant/xexecscript.d/vagrant.sh
+++ vagrant/xexecscript.d/vagrant.sh
@@ -9,5 +9,5 @@ declare chroot_dir=$1
 
 chroot $1 $SHELL -ex <<'EOS'
   # don't use 1.4.2. https://groups.google.com/forum/#!topic/vagrant-up/HNXKTBv0-Jw
-  rpm -ivh https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.1_$(arch).rpm
+  rpm -ivh https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_$(arch).rpm
 EOS
