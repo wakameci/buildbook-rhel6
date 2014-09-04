41f78176ae69ac29252657fa75cfe595724cd17a
Masahito Yoshida <masahito@axsh.net>
2014-09-04 15:46:25 +0900

    add baseurl parameter.

--- hold-releasever.hold-baseurl/xexecscript.d/hold-baseurl.sh
+++ hold-releasever.hold-baseurl/xexecscript.d/hold-baseurl.sh
@@ -13,16 +13,18 @@ if [[ -f ${chroot_dir}/etc/yum/vars/releasever ]]; then
 
   mv ${chroot_dir}/etc/yum.repos.d/CentOS-Base.repo{,.saved}
 
+  baseurl=http://ftp.riken.jp/Linux/centos
+
   cat <<-REPO > ${chroot_dir}/etc/yum.repos.d/CentOS-Base.repo
 	[base]
 	name=CentOS-\$releasever - Base
-	baseurl=http://ftp.riken.jp/Linux/centos/\$releasever/os/\$basearch/
+	baseurl=${baseurl}/\$releasever/os/\$basearch/
 	gpgcheck=1
 	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${majorver}
 
 	[updates]
 	name=CentOS-\$releasever - Updates
-	baseurl=http://ftp.riken.jp/Linux/centos/\$releasever/updates/\$basearch/
+	baseurl=${baseurl}/\$releasever/updates/\$basearch/
 	gpgcheck=1
 	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${majorver}
 	REPO
