989a2e2b49d76460bad9c38f8733e8b9ec3f4c25
Masahito Yoshida <masahito@axsh.net>
2014-10-18 17:09:27 +0900

    make sure to update ca-certificates.

--- epel-release/xexecscript.d/epel-release.sh
+++ epel-release/xexecscript.d/epel-release.sh
@@ -8,5 +8,11 @@ set -e
 declare chroot_dir=$1
 
 chroot $1 $SHELL -ex <<'EOS'
-  rpm -qi epel-release-6-8 >/dev/null || { yum install --disablerepo=updates -y http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm; }
+  rpm -qi epel-release-6-8 >/dev/null || {
+    yum install --disablerepo=updates -y http://ftp.riken.jp/Linux/fedora/epel/6/i386/epel-release-6-8.noarch.rpm
+
+    # in order escape below error
+    # > Error: Cannot retrieve metalink for repository: epel. Please verify its path and try again
+    yum install -y ca-certificates
+  }
 EOS
