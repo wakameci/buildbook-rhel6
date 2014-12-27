2d0296e3b14773692e2c3953586dff9dd9e52938
Masahito Yoshida <masahito@axsh.net>
2014-12-28 01:21:51 +0900

    raw.github.com -> raw.githubusercontent.com

--- openvnet.repo/xexecscript.d/openvnet.repo.sh
+++ openvnet.repo/xexecscript.d/openvnet.repo.sh
@@ -9,10 +9,10 @@ set -o pipefail
 declare chroot_dir=$1
 
 chroot $1 $SHELL -ex <<'EOS'
-  until curl -fsSkL -o /etc/yum.repos.d/openvnet.repo https://raw.github.com/axsh/openvnet/master/openvnet.repo; do
+  until curl -fsSkL -o /etc/yum.repos.d/openvnet.repo https://raw.githubusercontent.com/axsh/openvnet/master/openvnet.repo; do
     sleep 1
   done
-  until curl -fsSkL -o /etc/yum.repos.d/openvnet-third-party.repo https://raw.github.com/axsh/openvnet/master/openvnet-third-party.repo; do
+  until curl -fsSkL -o /etc/yum.repos.d/openvnet-third-party.repo https://raw.githubusercontent.com/axsh/openvnet/master/openvnet-third-party.repo; do
     sleep 1
   done
 EOS
