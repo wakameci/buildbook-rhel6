556226438767c2a6377a6640d28fdfdbac902755
Masahito Yoshida <masahito@axsh.net>
2015-01-06 19:44:00 +0900

    support i386 based package installation.

--- libyaml/xexecscript.d/libyaml.sh
+++ libyaml/xexecscript.d/libyaml.sh
@@ -11,10 +11,18 @@ declare chroot_dir=$1
 chroot $1 $SHELL -ex <<'EOS'
   releasever=$(< /etc/yum/vars/releasever)
 
+  arch=$(arch)
+  case "${arch}" in
+    i*86)   basearch=i386 arch=i686 ;;
+    x86_64) basearch=${arch} ;;
+  esac
+
   case "${releasever}" in
     6.[0-5])
       for pkg_name in libyaml libyaml-devel; do
-        rpm -qa ${pkg_name} | egrep -q ${pkg_name} || { yum install -y http://ftp.jaist.ac.jp/pub/Linux/CentOS/6.6/os/x86_64/Packages/${pkg_name}-0.1.3-1.4.el6.x86_64.rpm; }
+        rpm -qa ${pkg_name} | egrep -q ${pkg_name} || {
+          yum install -y http://ftp.jaist.ac.jp/pub/Linux/CentOS/6.6/os/${basearch}/Packages/${pkg_name}-0.1.3-1.4.el6.${arch}.rpm
+        }
       done
       ;;
     *)
