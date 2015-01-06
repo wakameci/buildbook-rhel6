ac1635970356e013393b3514d0583c1875463bd4
Masahito Yoshida <masahito@axsh.net>
2015-01-06 19:54:22 +0900

    support i386 based package installation.

--- anti-shellshock/xexecscript.d/anti-shellshock.sh
+++ anti-shellshock/xexecscript.d/anti-shellshock.sh
@@ -11,10 +11,16 @@ declare chroot_dir=$1
 chroot $1 $SHELL -ex <<'EOS'
   releasever=$(< /etc/yum/vars/releasever)
 
+  arch=$(arch)
+  case "${arch}" in
+    i*86)   basearch=i386 arch=i686 ;;
+    x86_64) basearch=${arch} ;;
+  esac
+
   case "${releasever}" in
     6.5)
       yum update  -y bash ;;
     6.[0-4])
-      yum install -y http://ftp.jaist.ac.jp/pub/Linux/CentOS/6.5/updates/x86_64/Packages/bash-4.1.2-15.el6_5.2.x86_64.rpm ;;
+      yum install -y http://http://vault.centos.org/6.5/updates/${basearch}/Packages/bash-4.1.2-15.el6_5.2.${arch}.rpm ;;
   esac
 EOS
