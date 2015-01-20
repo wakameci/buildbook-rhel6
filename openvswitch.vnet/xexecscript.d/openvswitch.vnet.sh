0143767d2ef478cbad427023009bd19f32a5a0a6
Masahito Yoshida <masahito@axsh.net>
2015-01-20 17:34:29 +0900

    update ovs version to 2.3.1.

--- openvswitch.vnet/xexecscript.d/openvswitch.vnet.sh
+++ openvswitch.vnet/xexecscript.d/openvswitch.vnet.sh
@@ -12,11 +12,12 @@ chroot $1 $SHELL -ex <<'EOS'
   releasever=$(< /etc/yum/vars/releasever)
 
   case "${releasever}" in
-    # xxx HOTFIX: Open vSwitch doesn't support CentOS 6.6. xxx
-    6.6)
-      releasever=6.5
+    *)
       ;;
   esac
-  yum install --disablerepo=updates -y http://dlc.openvnet.axsh.jp/packages/rhel/openvswitch/${releasever}/kmod-openvswitch-2.3.0-1.el6.x86_64.rpm
-  yum install --disablerepo=updates -y http://dlc.openvnet.axsh.jp/packages/rhel/openvswitch/${releasever}/openvswitch-2.3.0-1.x86_64.rpm
+
+  openvswitch_version=2.3.1
+
+  yum install --disablerepo=updates -y http://dlc.openvnet.axsh.jp/packages/rhel/openvswitch/${releasever}/kmod-openvswitch-${openvswitch_version}-1.el6.x86_64.rpm
+  yum install --disablerepo=updates -y http://dlc.openvnet.axsh.jp/packages/rhel/openvswitch/${releasever}/openvswitch-${openvswitch_version}-1.x86_64.rpm
 EOS
