edd7091f56d302687e097ac008945510f79bb88d
Masahito Yoshida <masahito@axsh.net>
2015-01-28 16:22:01 +0900

    fix gcc installation for 6.6.

--- jenkins.core/xexecscript.d/build-tools.sh
+++ jenkins.core/xexecscript.d/build-tools.sh
@@ -10,9 +10,37 @@ declare chroot_dir=$1
 
 chroot $1 $SHELL -ex <<'EOS'
   yum install --disablerepo=updates -y \
-    qemu-kvm qemu-img \
-    parted kpartx \
-    gcc gcc-c++ \
-    rpm-build automake createrepo \
+    qemu-kvm qemu-img
+  yum install --disablerepo=updates -y \
+    parted kpartx
+  yum install --disablerepo=updates -y \
+    rpm-build automake createrepo
+  yum install --disablerepo=updates -y \
     openssl-devel zlib-devel readline-devel
+EOS
+
+# "gcc" depends on glibc* which is pached in "ghost-buster".
+#
+# 6.6)
+#   "ghost-buster" updates glibc-* using updates yum repository.
+#   because gcc must not disable updates yum repository.
+
+chroot $1 $SHELL -ex <<'EOS'
+  releasever=$(< /etc/yum/vars/releasever)
+
+  case "${releasever}" in
+    6.6)
+      yum install -y \
+        gcc
+      ;;
+    *)
+      yum install --disablerepo=updates -y \
+        gcc
+      ;;
+  esac
+EOS
+
+chroot $1 $SHELL -ex <<'EOS'
+  yum install --disablerepo=updates -y \
+    gcc-c++
 EOS
