cbdaa004e4f9ad3d55836cd4a0b10868a10fb3b0
Masahito Yoshida <masahito@axsh.net>
2015-03-09 21:22:36 +0900

    use proper indent.

--- hubot.devel/xexecscript.d/hubot.devel.sh
+++ hubot.devel/xexecscript.d/hubot.devel.sh
@@ -9,5 +9,5 @@ set -o pipefail
 declare chroot_dir=$1
 
 chroot $1 $SHELL -ex <<'EOS'
- yum install --disablerepo=updates -y redis libicu-devel
+  yum install --disablerepo=updates -y redis libicu-devel
 EOS
