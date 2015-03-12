df531a728c610e22a33ab264b4caf8a5411e8c5e
Masahito Yoshida <masahito@axsh.net>
2015-03-12 19:29:15 +0900

    first run-book.sh.

---
+++ run-book.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+
+declare abs_dirname=${BASH_SOURCE[0]%/*}/
+declare name=$1
+declare chroot_dir=${2:-/}
+
+if [[ -z "${name}" ]]; then
+  cat <<-EOS >&2
+	USAGE
+	  $ run-book.sh <name> <chroot-dir>
+	EOS
+  exit 1
+fi
+
+cd ${abs_dirname}
+
+[[ -d ${name} ]]
+
+# TODO: support copy.txt deployment.
+
+while read line; do
+  echo eval ${line} ${chroot_dir}
+done < <(find -L ${name} ! -type d -perm -a=x | sort)
