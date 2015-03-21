c6efd262edebf31c38b174b025d75027b2dd28a3
Masahito Yoshida <masahito@axsh.net>
2015-03-21 17:36:56 +0900

    add configure_sudo_sudoers().

--- jenkins.core/xexecscript.d/jenkins-account.sh
+++ jenkins.core/xexecscript.d/jenkins-account.sh
@@ -18,4 +18,18 @@ chroot $1 $SHELL -ex <<'EOS'
   getent passwd jenkins
 EOS
 
+function configure_sudo_sudoers() {
+  local chroot_dir=$1 user_name=$2 tag_specs=${3:-"NOPASSWD:"}
+  [[ -d "${chroot_dir}" ]] || { echo "[ERROR] directory not found: ${chroot_dir} (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  [[ -n "${user_name}"  ]] || { echo "[ERROR] Invalid argument: user_name:${user_name} (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
+  #
+  # Tag_Spec ::= ('NOPASSWD:' | 'PASSWD:' | 'NOEXEC:' | 'EXEC:' |
+  #               'SETENV:' | 'NOSETENV:' | 'LOG_INPUT:' | 'NOLOG_INPUT:' |
+  #               'LOG_OUTPUT:' | 'NOLOG_OUTPUT:')
+  #
+  # **don't forget suffix ":" to tag_specs.**
+  #
+  egrep ^${user_name} -w ${chroot_dir}/etc/sudoers || { echo "${user_name} ALL=(ALL) ${tag_specs} ALL" >> ${chroot_dir}/etc/sudoers; }
+}
+
 configure_sudo_sudoers $1 jenkins NOPASSWD:
