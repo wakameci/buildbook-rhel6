400725ccfb16d277804b923852e9dec7b699b583
Masahito Yoshida <masahito@axsh.net>
2015-03-22 17:01:58 +0900

    update mirror site baseurl.

--- hold-releasever.hold-baseurl/xexecscript.d/hold-baseurl.sh
+++ hold-releasever.hold-baseurl/xexecscript.d/hold-baseurl.sh
@@ -15,7 +15,7 @@ function baseurl() {
 
   case "${releasever}" in
     6.6 )
-      baseurl=http://ftp.riken.jp/Linux/centos
+      baseurl=http://ftp.jaist.ac.jp/pub/Linux/CentOS
       ;;
   esac
 
