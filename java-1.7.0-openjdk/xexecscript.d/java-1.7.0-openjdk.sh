a57e8b91900d829a93b1db8318b902b649b08ad3
Masahito Yoshida <masahito@axsh.net>
2015-05-04 22:31:29 +0900

    add java-1.7.0-openjdk set.

---
+++ java-1.7.0-openjdk/xexecscript.d/java-1.7.0-openjdk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+
+declare chroot_dir=$1
+
+chroot $1 $SHELL -ex <<'EOS'
+  yum install --disablerepo=updates -y java-1.7.0-openjdk
+EOS
